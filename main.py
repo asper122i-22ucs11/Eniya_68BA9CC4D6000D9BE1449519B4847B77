1d7a08422af2a980ff7cab2a0f009e7403772671
asper122i-22ucs11 <144411455+asper122i-22ucs11@users.noreply.github.com>
2023-09-14 16:40:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+class bank account:
+def__initial__(self,account_number,account_holder_name,initial_balance=0.0):
+self.__account_number=account_number
+self.__account_holder_name=account _holder_name
+self.__account_balance=initial_balance
+def deposit(self,amount):
+  if amount>0:
+  self.__account_balance+=amount
+  print("deposited₹{}.new balance.₹{}".format (amount,self.__account_balance))
+  else:
+     print("invalid deposit amount")
+def display_balance(self):
+  print("account balance for{} account ₹():₹".format(self.__account_number,self.__account_balance))
+    account =Bank account(account_number="123456789", account _holder_name="hari prabu",initial_balance()
+    account.display_balance()
+    account.deposit(500.0)
+    account.withdraw(200.0)
+    account.display_balance()
